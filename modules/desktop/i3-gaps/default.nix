a3a5be64ed00f924ba4b5aef4236c18a6b4ddb53
douglastofoli <tofoli.douglas@hotmail.com>
2022-08-08 18:23:31 -0300

    create config for i3-gaps

---
+++ modules/desktop/i3-gaps/default.nix
@@ -0,0 +1,72 @@
+{ config, lib, pkgs, protocol, ... }:
+
+{
+  config = lib.mkIf (protocol == "X") {
+    programs.dconf.enable = true;
+
+    services = {
+      xserver = {
+        enable = true;
+
+        layout = "br";
+        xkbOptions = "eurosign:e";
+
+        displayManager = {
+          lightdm = {
+            enable = true;
+            background =
+              pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath;
+            greeters = {
+              gtk = {
+                theme = {
+                  name = "Dracula";
+                  package = pkgs.dracula-theme;
+                };
+                cursorTheme = {
+                  name = "Dracula-cursors";
+                  package = pkgs.dracula-theme;
+                  size = 16;
+                };
+              };
+            };
+          };
+          defaultSession = "i3";
+        };
+
+        windowManager = {
+          i3 = {
+            enable = true;
+            package = pkgs.i3-gaps;
+            extraPackages = with pkgs; [ i3status ];
+            # configFile = ./i3/config;
+          };
+        };
+
+        serverFlagsSection = ''
+          Option "BlankTime" "0"
+          Option "StandbyTime" "0"
+          Option "SuspendTime" "0"
+          Option "OffTime" "0"
+        '';
+
+        resolutions = [
+          {
+            x = 1920;
+            y = 1080;
+          }
+          {
+            x = 2560;
+            y = 1080;
+          }
+        ];
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      xclip
+      xorg.xev
+      xorg.xkill
+      xorg.xrandr
+    ];
+  };
+}

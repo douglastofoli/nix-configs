eedd8c5147acea44329e624ea8008aefc2366877
douglastofoli <tofoli.douglas@hotmail.com>
2022-08-11 14:17:11 -0300

    create rofi config

--- modules/programs/rofi.nix
+++ modules/programs/rofi.nix
@@ -40,98 +40,101 @@ in {
           padding = mkLiteral "1px";
         };
         "textbox" = { text-color = mkLiteral "@foreground"; };
-#listview {
-    fixed-height: 0;
-    border:       2px dash 0px 0px ;
-    border-color: @bordercolor;
-    spacing:      2px ;
-    scrollbar:    false;
-    padding:      2px 0px 0px ;
-}
-#element {
-    border:  0;
-    padding: 1px ;
-}
-#element.normal.normal {
-    background-color: @background-color;
-    text-color:       @foreground;
-}
-#element.normal.urgent {
-    background-color: @urgent-background;
-    text-color:       @urgent-foreground;
-}
-#element.normal.active {
-    background-color: @active-background;
-    text-color:       @foreground;
-}
-#element.selected.normal {
-    background-color: @selected-background;
-    text-color:       @foreground;
-}
-#element.selected.urgent {
-    background-color: @selected-urgent-background;
-    text-color:       @foreground;
-}
-#element.selected.active {
-    background-color: @selected-active-background;
-    text-color:       @foreground;
-}
-#element.alternate.normal {
-    background-color: @background-color;
-    text-color:       @foreground;
-}
-#element.alternate.urgent {
-    background-color: @urgent-background;
-    text-color:       @foreground;
-}
-#element.alternate.active {
-    background-color: @active-background;
-    text-color:       @foreground;
-}
-#scrollbar {
-    width:        2px ;
-    border:       0;
-    handle-width: 8px ;
-    padding:      0;
-}
-#sidebar {
-    border:       2px dash 0px 0px ;
-    border-color: @separatorcolor;
-}
-#button.selected {
-    background-color: @selected-background;
-    text-color:       @foreground;
-}
-#inputbar {
-    spacing:    0;
-    text-color: @foreground;
-    padding:    1px ;
-}
-#case-indicator {
-    spacing:    0;
-    text-color: @foreground;
-}
-#entry {
-    spacing:    0;
-    text-color: @foreground;
-}
-#prompt {
-    spacing:    0;
-    text-color: @foreground;
-}
-#inputbar {
-    children:   [ prompt,textbox-prompt-colon,entry,case-indicator ];
-}
-#textbox-prompt-colon {
-    expand:     false;
-    str:        ":";
-    margin:     0px 0.3em 0em 0em ;
-    text-color: @foreground;
-}
-element-text, element-icon {
-    background-color: inherit;
-    text-color: inherit;
-}
+        "listview" = {
+          fixed-height = 0;
+          border = mkLiteral "2px dash 0px 0px";
+          border-color = mkLiteral "@bordercolor";
+          spacing = mkLiteral "2px";
+          scrollbar = false;
+          padding = mkLiteral "2px 0px 0px";
+        };
+        "element" = {
+          border = 0;
+          padding = mkLiteral "1px";
+        };
+        "element normal normal" = {
+          background-color = mkLiteral "@background-color";
+          text-color = mkLiteral "@foreground";
+        };
+        "element normal urgent" = {
+          background-color = mkLiteral "@urgent-background";
+          text-color = mkLiteral "@urgente-foreground";
+        };
+        "element normal active" = {
+          background-color = mkLiteral "@active-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "element selected normal" = {
+          background-color = mkLiteral "@selected-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "element selected urgent" = {
+          background-color = mkLiteral "@selected-urgent-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "element selected active" = {
+          background-color = mkLiteral "@selected-active-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "element alternate normal" = {
+          background-color = mkLiteral "@background-color";
+          text-color = mkLiteral "@foreground";
+        };
+        "element alternate urgent" = {
+          background-color = mkLiteral "@urgent-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "element alternate active" = {
+          background-color = mkLiteral "@active-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "scrollbar" = {
+          width = mkLiteral "2px";
+          border = 0;
+          handle-width = mkLiteral "8px";
+          padding = 0;
+        };
+        "sidebar" = {
+          border = mkLiteral "2px dash 0px 0px";
+          border-color = mkLiteral "@separatorcolor";
+        };
+        "button selected" = {
+          background-color = mkLiteral "@selected-background";
+          text-color = mkLiteral "@foreground";
+        };
+        "inputbar" = {
+          spacing = 0;
+          text-color = mkLiteral "@foreground";
+          padding = mkLiteral "1px";
+          children =
+            mkLiteral "[ prompt, textbox-prompt, entry, case-indicator ]";
+        };
+        "case-indicator" = {
+          spacing = 0;
+          text-color = mkLiteral "@foreground";
+        };
+        "entry" = {
+          spacing = 0;
+          text-color = mkLiteral "@foreground";
+        };
+        "prompt" = {
+          spacing = 0;
+          text-color = mkLiteral "@foreground";
+        };
+        "textbox-prompt-colon" = {
+          expand = false;
+          str = mkLiteral ":";
+          margin = mkLiteral "0px 0.3em 0em 0em";
+          text-color = mkLiteral "@foreground";
+        };
+        "element-text" = {
+          background-color = mkLiteral "inherit";
+          text-color = mkLiteral "inherit";
+        };
+        "element-icon" = {
+          background-color = mkLiteral "inherit";
+          text-color = mkLiteral "inherit";
+        };
       };
     };
   };

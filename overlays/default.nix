7668b4d064f537dcb21b94e1eff4ad427f87c983
douglastofoli <tofoli.douglas@hotmail.com>
2022-08-15 23:12:32 -0300

    update discord overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,9 @@
 {
-  nixpkgs.overlays = [ # This overlay will pull the latest version of Discord
+  nixpkgs.overlays = [
     (self: super: {
       discord = super.discord.overrideAttrs (_: {
-        src = builtins.fetchTarball {
-          url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-          sha256 = "1bhjalv1c0yxqdra4gr22r31wirykhng0zglaasrxc41n0sjwx0m";
-        };
+        src = builtins.fetchTarball
+          "https://discord.com/api/download?platform=linux&format=tar.gz";
       });
     })
   ];

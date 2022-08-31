c1e0b304bc0fb11a4dfbd2653580a93fdd4c0683
douglastofoli <tofoli.douglas@hotmail.com>
2022-08-31 20:06:31 -0300

    create script to generate btrfs partitions

--- luks-btrfs.sh
+++ luks-btrfs.sh
@@ -2,38 +2,20 @@
 
 DISK=/dev/nvme0n1
 
-mkfs.fat -F 32 -n BOOT "$DISK"p1
-
 echo
 echo "Crypt with cryptsetup"
 echo
 cryptsetup --verify-passphrase -v luksFormat "$DISK"p2
 cryptsetup open "$DISK"p2 enc
 
-echo
-echo "Creating swap inside encrypted partition"
-echo
-pvcreate /dev/mapper/enc
-vgcreate lvm /dev/mapper/enc
-
-read -p "What is the swap size? Ex: 8G " swap_size
-lvcreate --size $swap_size --name swap lvm
-lvcreate --extents 100%FREE --name root lvm
-
 echo "Making file sistem..."
 echo
-mkswap /dev/lvm/swap
-mkfs.btrfs /dev/lvm/root
-
-echo
-echo "Mounting swap..."
-echo
-swapon /dev/lvm/swap
+mkfs.fat -F 32 -n BOOT "$DISK"p1
+mkfs.btrfs /dev/mapper/enc
 
 echo
 echo "Creating subvolumes..."
 echo
-
 read -p "Want to create subvolume ROOT? [Y/n]" subvol_root
 if [[ $subvol_root == "Y" || $subvol_root == "y" ]]; then
   btrfs subvolume create /mnt/root
@@ -46,34 +28,54 @@ read -p "Want to create subvolume NIX? [Y/n]" subvol_nix
 if [[ $subvol_nix == "Y" || $subvol_nix == "y" ]]; then
   btrfs subvolume create /mnt/nix
 fi
+read -p "Want to create subvolume SWAP? [Y/n]" subvol_swap
+if [[ $subvol_swap == "Y" || $subvol_swap == "y" ]]; then
+  btrfs subvolume create /mnt/swap
+fi
 
 umount /mnt
 
 echo
 echo "Mounting directories..."
 echo
-
 if [[ $subvol_root == "Y" || $subvol_root == "y" ]]; then
-  mount -o subvol=root,compress=zstd /dev/lvm/root /mnt
+  mount -o subvol=root,compress=zstd /dev/mapper/enc /mnt
 fi
 if [[ $subvol_home == "Y" || $subvol_home == "y" ]]; then
   mkdir /mnt/home
-  mount -o subvol=home,compress=zstd /dev/lvm/root /mnt/home
+  mount -o subvol=home,compress=zstd /dev/mapper/enc /mnt/home
 fi
 if [[ $subvol_nix == "Y" || $subvol_nix == "y" ]]; then
   mkdir /mnt/nix
-  mount -o subvol=nix,compress=zstd,noatime /dev/lvm/root /mnt/nix
+  mount -o subvol=nix,compress=zstd,noatime /dev/mapper/enc /mnt/nix
+fi
+if [[ $subvol_swap == "Y" || $subvol_swap == "y" ]]; then
+  mkdir /mnt/swap
+  mount -o subvol=swap /dev/mapper/enc /mnt/swap
 fi
 
+echo
+echo "Mounting boot partition..."
+echo
 mkdir /mnt/boot
 mount "$DISK"p1 /mnt/boot
 
 echo
+echo "Creating swap file..."
+echo
+truncate -s 0 /mnt/swap/swapfile
+chattr +C /mnt/swap/swapfile
+btrfs property set /mnt/swap/swapfile compression none
+dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8192
+chmod 0600 /mnt/swap/swapfile
+mkswap /mnt/swap/swapfile
+
+echo
+echo "Mounting swap..."
+echo
+swapon /mnt/swap/swapfile
+
+echo
 echo "Generation NixOs config"
-
+echo
 nixos-generate-config --root /mnt
-
-nix-env -iA nixos.git
-
-cd /mnt/etc/nixos
-git clone https://github.com/douglastofoli/nix-configs

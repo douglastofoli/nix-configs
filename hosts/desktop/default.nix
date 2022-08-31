e1f970148e36e7a033eb71284d75bac7484875c5
douglastofoli <tofoli.douglas@hotmail.com>
2022-08-30 22:29:08 -0300

    fix boot options

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -15,25 +15,35 @@
     kernelPackages = pkgs.linuxPackages_latest;
     supportedFilesystems = [ "btrfs" ];
 
-    loader = {
+    loader. = {
       efi.canTouchEfiVariables = true;
       efiSysMountPoint = "/boot";
+
+      grub = {
+        enable = true;
+        efiSupport = true;
+        enableCryptodisk = true;
+        devices = [ "nodev" ];
+        device = "nodev";
+        version = 2;
+        extraEntries = ''
+          menuentry "Windows 11" {
+            insmod part_gpt
+            insmod fat
+            insmod search_fs_uuid
+            insmod chain
+            search --fs-uuid --set=root "0017-8B44"
+            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+          }
+        '';
+      };
     };
-    grub = {
-      enable = true
-      efiSupport = true;
-      devices = [ "nodev" ];
-      extraEntries = ''
-        menuentry "Windows 11" {
-          insmod part_gpt
-          insmod fat
-          insmod search_fs_uuid
-          insmod chain
-          search --fs-uuid --set=root UUID_AQUI
-          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-        }
-      '';
-      version = 2;
+    
+    initrd.luks.devices = {
+      root = {
+        device = "/dev/disk/by-uuid/4a4b273e-1008-405b-9e24-e3e24adbe5c9";
+        preLVM = true;
+      };
     };
   };
 

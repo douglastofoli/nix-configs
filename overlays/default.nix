0530887559c800a8360e8a7ca6c0872864cf4a28
douglastofoli <tofoli.douglas@hotmail.com>
2022-09-10 15:05:07 -0300

    remove comments

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,30 +10,16 @@
         };
       });
     })
-    (self: super: {
-      insync-v3 = super.insync-v3.overrideAttrs (old: rec {
-        version = "3.7.11.50381";
+    # (self: super: {
+    #   insync-v3 = super.insync-v3.overrideAttrs (_: {
+    #     version = "3.7.11.50381";
 
-        buildInputs = old.buildInputs
-          ++ [ pkgs.xorg.libxcb pkgs.libxkbcommon pkgs.libdrm ];
-
-        src = builtins.fetchurl {
-          url =
-            "https://cdn.insynchq.com/builds/linux/insync_3.7.11.50381-focal_amd64.deb";
-          sha256 = "14vm7nck0rnnxz6mpzx2cclycbyj1s4rc3rwwjk4yx8m1y6i91jv";
-        };
-
-        installPhase = ''
-          mkdir -p $out/bin $out/lib $out/share
-          cp -R usr/* $out/
-          rm $out/lib/insync/libGLX.so.0
-          rm $out/lib/insync/libdrm*.so*
-          rm $out/lib/insync/libxkbcommon*.so*
-          rm $out/lib/insync/libQt5*
-          sed -i 's|/usr/lib/insync|/lib/insync|' "$out/bin/insync"
-          wrapQtApp "$out/lib/insync/insync"
-        '';
-      });
-    })
+    #     src = builtins.fetchurl {
+    #       url =
+    #         "https://cdn.insynchq.com/builds/linux/insync_3.7.11.50381-focal_amd64.deb";
+    #       sha256 = "14vm7nck0rnnxz6mpzx2cclycbyj1s4rc3rwwjk4yx8m1y6i91jv";
+    #     };
+    #   });
+    # })
   ];
 }

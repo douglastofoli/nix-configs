9accd32dddfa384a17ef9b7e2ed746d104e09310
douglastofoli <tofoli.douglas@hotmail.com>
2022-09-10 15:05:46 -0300

    change script to fresh install

--- luks-btrfs.sh
+++ luks-btrfs.sh
@@ -2,71 +2,31 @@
 
 DISK=/dev/nvme0n1
 
-echo
-echo "Crypt with cryptsetup"
-echo
 cryptsetup --verify-passphrase -v luksFormat "$DISK"p2
 cryptsetup open "$DISK"p2 enc
 
-echo "Making file sistem..."
-echo
-mkfs.fat -F 32 -n BOOT "$DISK"p1
+mkfs.fat -F 32 "$DISK"p1
 mkfs.btrfs /dev/mapper/enc
 
 mount -t btrfs /dev/mapper/enc /mnt
 
-echo
-echo "Creating subvolumes..."
-echo
-read -p "Want to create subvolume ROOT? [Y/n]" subvol_root
-if [[ $subvol_root == "Y" || $subvol_root == "y" ]]; then
-  btrfs subvolume create /mnt/root
-fi
-read -p "Want to create subvolume HOME? [Y/n]" subvol_home
-if [[ $subvol_home == "Y" || $subvol_home == "y" ]]; then
-  btrfs subvolume create /mnt/home
-fi
-read -p "Want to create subvolume NIX? [Y/n]" subvol_nix
-if [[ $subvol_nix == "Y" || $subvol_nix == "y" ]]; then
-  btrfs subvolume create /mnt/nix
-fi
-read -p "Want to create subvolume SWAP? [Y/n]" subvol_swap
-if [[ $subvol_swap == "Y" || $subvol_swap == "y" ]]; then
-  btrfs subvolume create /mnt/swap
-fi
-
-btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/nix
+btrfs subvolume create /mnt/swap
 
 umount /mnt
 
-echo
-echo "Mounting directories..."
-echo
-if [[ $subvol_root == "Y" || $subvol_root == "y" ]]; then
-  mount -o subvol=root,compress=zstd /dev/mapper/enc /mnt
-fi
-if [[ $subvol_home == "Y" || $subvol_home == "y" ]]; then
-  mkdir /mnt/home
-  mount -o subvol=home,compress=zstd /dev/mapper/enc /mnt/home
-fi
-if [[ $subvol_nix == "Y" || $subvol_nix == "y" ]]; then
-  mkdir /mnt/nix
-  mount -o subvol=nix,compress=zstd,noatime /dev/mapper/enc /mnt/nix
-fi
-if [[ $subvol_swap == "Y" || $subvol_swap == "y" ]]; then
-  mkdir /mnt/swap
-  mount -o subvol=swap /dev/mapper/enc /mnt/swap
-fi
+mount -o compress=zstd,subvol=root /dev/mapper/enc /mnt
+mkdir /mnt/{home,nix,swap}
+mount -o compress=zstd,subvol=home /dev/mapper/enc /mnt/home
+mount -o compress=zstd,noatime,subvol=nix /dev/mapper/enc /mnt/nix
+mount -o subvol=swap /dev/mapper/enc /swap
 
-echo
-echo "Mounting boot partition..."
-echo
 mkdir /mnt/boot
 mount "$DISK"p1 /mnt/boot
 
-echo
-echo "Creating swap file..."
-echo
+# Create swapfile
 truncate -s 0 /mnt/swap/swapfile
 chattr +C /mnt/swap/swapfile
 btrfs property set /mnt/swap/swapfile compression none
@@ -74,12 +34,6 @@ dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8192
 chmod 0600 /mnt/swap/swapfile
 mkswap /mnt/swap/swapfile
 
-echo
-echo "Mounting swap..."
-echo
 swapon /mnt/swap/swapfile
 
-echo
-echo "Generation NixOs config"
-echo
 nixos-generate-config --root /mnt

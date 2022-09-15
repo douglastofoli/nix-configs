7147b8df6acabaea0727a0a84c6e381e47877f4a
douglastofoli <tofoli.douglas@hotmail.com>
2022-09-14 21:50:04 -0300

    remove swap

--- luks-btrfs.sh
+++ luks-btrfs.sh
@@ -13,7 +13,6 @@ mount -t btrfs /dev/mapper/enc /mnt
 btrfs subvolume create /mnt/root
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/nix
-btrfs subvolume create /mnt/swap
 
 umount /mnt
 
@@ -21,19 +20,8 @@ mount -o compress=zstd,subvol=root /dev/mapper/enc /mnt
 mkdir /mnt/{home,nix,swap}
 mount -o compress=zstd,subvol=home /dev/mapper/enc /mnt/home
 mount -o compress=zstd,noatime,subvol=nix /dev/mapper/enc /mnt/nix
-mount -o subvol=swap /dev/mapper/enc /mnt/swap
 
 mkdir /mnt/boot
 mount "$DISK"p1 /mnt/boot
-
-# Create swapfile
-truncate -s 0 /mnt/swap/swapfile
-chattr +C /mnt/swap/swapfile
-btrfs property set /mnt/swap/swapfile compression none
-dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8192
-chmod 0600 /mnt/swap/swapfile
-mkswap /mnt/swap/swapfile
-
-swapon /mnt/swap/swapfile
 
 nixos-generate-config --root /mnt

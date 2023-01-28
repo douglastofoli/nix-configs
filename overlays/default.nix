01f83974afb8622a81c9bd330c860669e7a9bc7e
douglastofoli <tofoli.douglas@hotmail.com>
2023-01-28 11:31:54 -0300

    add overlay for discord

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,17 +6,7 @@
       discord = super.discord.overrideAttrs (_: {
         src = builtins.fetchTarball {
           url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-          sha256 = "1kwqn1xr96kvrlbjd14m304g2finc5f5ljvnklg6fs5k4avrvmn4";
-        };
-      });
-    })
-    (self: super: {
-      insync-v3 = super.insync-v3.overrideAttrs (_: {
-        version = "3.7.11.50381";
-        src = super.fetchurl {
-          url =
-            "https://cdn.insynchq.com/builds/linux/insync_3.7.11.50381-focal_amd64.deb";
-          sha256 = "sha256-W4YUjQ8VdU+m5DwPlokO0i/mKWOi/1vN79ZmMJk9dZM=";
+          sha256 = "087p8z538cyfa9phd4nvzjrvx4s9952jz1azb2k8g6pggh1vxwm8";
         };
       });
     })

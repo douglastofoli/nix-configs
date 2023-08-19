a14ac14db228fcf97822805332854e6c630c6dd5
douglastofoli <tofoli.douglas@hotmail.com>
2023-08-19 11:49:06 -0300

    add shell flutter

---
+++ shells/flutter/emulator.nix
@@ -0,0 +1,10 @@
+with import <nixpkgs> {};
+
+androidenv.emulateApp {
+  name = "Pixel_4_API_29";
+  platformVersion = "29";
+  abiVersion = "x86";
+  enableGPU = true;
+  avdHomeDir = "/home/douglas/.android/avd";
+  systemImageType = "google_apis";
+}

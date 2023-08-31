9aff65f81dabd06267c1993f8bef18a6fc60406b
douglastofoli <tofoli.douglas@hotmail.com>
2023-08-31 18:52:03 -0300

    add new package

--- shells/python/shell.nix
+++ shells/python/shell.nix
@@ -2,16 +2,35 @@
 
 with pkgs;
 
-let 
+let
   projectName = "python";
-  pythonPackages = ps: with ps; [
-    requests
-  ];
+  pythonPackages = ps:
+    with ps;
+    [
+      (buildPythonPackage rec {
+        pname = "databases";
+        version = "0.8.0";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-ZUTYLpkm8jPWlOwpzQGEA0RMf7boY6+IGoME0f9c+5A=";
+        };
+        doCheck = false;
+        propagatedBuildInputs = [
+          (buildPythonPackage rec {
+            pname = "SQLAlchemy";
+            version = "1.4.49";
+            src = fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-Bv8ly64ww5bEt3N0ZPKn/Deme32kCZk7GCsCTOyArtk=";
+            };
+            doCheck = false;
+            propagatedBuildInputs = [ pkgs.python311Packages.greenlet ];
+          })
+        ];
+      })
+    ];
 in mkShell {
   name = "${projectName}-shell";
 
-  packages = [
-    (python3.withPackages
-      pythonPackages)
-  ];
+  packages = [ (python3.withPackages pythonPackages) ];
 }

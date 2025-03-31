e5f93ca0b71cace0a4d0f69fdc6a5805e398a2d5
douglastofoli <tofoli.douglas@hotmail.com>
2025-03-31 13:26:27 -0300

    update shell elixir

--- shells/elixir/flake.nix
+++ shells/elixir/flake.nix
@@ -6,32 +6,48 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      inherit (pkgs.lib) optional optionals;
-      pkgs = import nixpkgs {inherit system;};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (pkgs.lib) optional optionals;
+        pkgs = import nixpkgs { inherit system; };
 
-      beamPkg = pkgs.beam.packagesWith pkgs.erlangR26;
-      elixir = beamPkg.elixir.override {
-        version = "1.16.2";
-        sha256 = "sha256-NUYYxf73Fuk3FUoVFKTo6IN9QCTvzz5wNshIf/nitJA=";
-      };
+        beamPkg = pkgs.beam.packagesWith pkgs.erlang_27;
+        elixir = beamPkg.elixir.override {
+          version = "1.18.3";
+          sha256 = "sha256-jH+1+IBWHSTyqakGClkP1Q4O2FWbHx7kd7zn6YGCog0=";
+        };
 
-      inputs = with pkgs;
-        [elixir glibcLocales]
-        ++ optional stdenv.isLinux [inotify-tools]
-        ++ optional stdenv.isDarwin terminal-notifier
-        ++ optionals stdenv.isDarwin
-        (with darwin.apple_sdk.frameworks; [CoreFoundation CoreServices]);
-    in
-      with pkgs; {
-        devShells.default = mkShell {
-          name = "gym";
+        exercism-pkg = pkgs.exercism;
+
+        inputs =
+          with pkgs;
+          [
+            elixir
+            glibcLocales
+          ]
+          ++ optional stdenv.isLinux inotify-tools
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin (
+            with darwin.apple_sdk.frameworks;
+            [
+              CoreFoundation
+              CoreServices
+            ]
+          )
+          ++ [ exercism-pkg ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "project";
           packages = inputs;
         };
-      });
+      }
+    );
 }

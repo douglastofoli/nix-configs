8a76c250c07633de03f989be011216e5c9bd956c
douglastofoli <tofoli.douglas@hotmail.com>
2025-06-09 22:16:28 -0300

    nix flake update

--- modules/services/yubikey.nix
+++ modules/services/yubikey.nix
@@ -10,6 +10,13 @@
     enable = true;
     enableSSHSupport = true;
     pinentryPackage = pkgs.pinentry-gtk2;
+
+    settings = {
+      default-cache-ttl = 86400;
+      max-cache-ttl = 604800;
+      default-cache-ttl-ssh = 86400;
+      max-cache-ttl-ssh = 604800;
+    };
   };
 
   environment.systemPackages = [

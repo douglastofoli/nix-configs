113f94402266b5ab257756ddab9362badcd5a9a6
Douglas Tofoli <tofoli.douglas@hotmail.com>
2026-03-02 21:00:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 DISK=/dev/nvme0n1
 
 parted $DISK -- mklabel gpt
-parted $DISK -- mkpart primary 512MB 100%
+parted $DISK -- mkpart root 512MB 100%
 parted $DISK -- mkpart ESP fat32 1MB 512MB
 parted $DISK -- set 2 esp on
 
@@ -11,7 +11,7 @@ mkfs.fat -F 32 -n boot ${DISK}p2
 mount /dev/disk/by-label/nixos /mnt
 
 mkdir -p /mnt/boot
-mount /dev/disk/by-label/boot /mnt/boot
+mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 
 nixos-generate-config --root /mnt
 
@@ -21,5 +21,7 @@ cd /mnt/etc/nixos
 
 git clone https://github.com/douglastofoli/nix-configs
 cd nix-configs
+
+cp /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/nix-configs/hosts/desktop/hardware-configuration.nix
 
 #nixos-install --flake .#desktop
